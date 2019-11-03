a2ec15f0f40be0a600670737aef3fafd2da74f33
Вадим Денисовский <vadim.denisovski@live.com>
2019-11-04 00:20:34 +0300

    Add project files.

---
+++ ConsoleApp5/Circle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Circle
+    {
+        struct Coords
+        {
+            public int x, y;
+        }
+
+        private Coords Center;
+        private double Radius;
+        private const double pi = 3.14;
+        private double circum;
+
+        public Circle() // конструктор по умолчанию
+        {
+            Center.x = 0;
+            Center.y = 0;
+
+            Radius = 1;
+
+            Circumference();
+        }
+
+        public Circle(int x, int y, int r) // параметризированный конструктор
+        {
+            Center.x = x;
+            Center.y = y;
+           
+            Radius = r;          
+
+            Circumference();
+        }
+
+        public int GetCenterX() => Center.x; // Свойства для доступа к полям класса (только для чтения).
+        public int GetCenterY() => Center.y; // Свойства для доступа к полям класса (только для чтения).
+        public double GetRadius() => Radius; // Свойства для доступа к полям класса (только для чтения).
+
+        public void Circumference() // определяем длину окружности
+        {
+            circum = 2 * Radius * pi;
+        }
+
+        public double GetCircumference() => circum;
+
+        public void ChangeCoords(int x, int y) // метод для смены местаположения центра окружности
+        {
+            Center.x += x;
+            Center.y += y;
+        }
+
+        public void ChangeRadius(int r) // метод для изминения радиуса
+        {
+            Radius += r;
+        }
+
+        public bool OneQuarterCoords() // метод для определения лежит ли окружность в одной координатной четверти
+        {
+            if (Center.x > 0 && Center.y > 0 && Radius < Center.x && Radius < Center.y)
+                return true;
+            else if (Center.x > 0 && Center.y < 0 && Radius < Center.x && Radius < Math.Abs(Center.y))
+                return true;
+            else if (Center.x < 0 && Center.y > 0 && Radius < Math.Abs(Center.x) && Radius < Center.y)
+                return true;
+            else if (Center.x < 0 && Center.y < 0 && Radius < Math.Abs(Center.x) && Radius < Math.Abs(Center.y))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// В метод CircleIntersection передаем обекты класса.
+        /// В этом методе определяем пересечение окружностей
+        /// две окружности пересекаются тогда и только тогда,
+        /// когда расстояния между центрами меньше суммы их радиусов, но больше модуля их разности, 
+        /// что мы и проверяем if-ом
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+
+        public static bool CircleIntersection(Circle c1, Circle c2) // определяем пересечение окружностей
+        {
+            if (Math.Sqrt((c1.GetCenterX() - c2.GetCenterX()) * (c1.GetCenterX() - c2.GetCenterX()) +
+                (c1.GetCenterY() - c2.GetCenterY()) * (c1.GetCenterY() - c2.GetCenterY())) < c1.GetRadius() + c2.GetRadius() &&
+                Math.Sqrt((c1.GetCenterX() - c2.GetCenterX()) * (c1.GetCenterX() - c2.GetCenterX()) +
+                (c1.GetCenterY() - c2.GetCenterY()) * (c1.GetCenterY() - c2.GetCenterY())) < Math.Abs(c1.GetRadius() - c2.GetRadius()))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+
+            Console.WriteLine("Первая окружность задается по умолчанию.");
+            
+            Console.WriteLine("Введите координаты 2-ой окружности.");
+            Console.Write("x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите радиус второй окружности: ");
+            int r2 = Convert.ToInt32(Console.ReadLine());
+            bool flag2 = true;
+            while(flag2)
+            {
+                if (r2 <= 0)
+                {
+                    Console.WriteLine("Радиус должен быть больше 0");
+                    Console.Write("Введите радиус: ");
+                    r2 = Convert.ToInt32(Console.ReadLine());
+                    flag2 = true;
+                }
+                else
+                    flag2 = false;
+            }
+
+            Circle c2 = new Circle(x2, y2, r2);
+
+            Console.WriteLine("Введите координаты 3-ей окружности.");
+            Console.Write("x3: ");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y3: ");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите радиус 3-ей окружности: ");
+            int r3 = Convert.ToInt32(Console.ReadLine());
+            bool flag3 = true;
+            while (flag3)
+            {
+                if (r3 <= 0)
+                {
+                    Console.WriteLine("Радиус должен быть больше 0");
+                    Console.Write("Введите радиус: ");
+                    r3 = Convert.ToInt32(Console.ReadLine());
+                    flag3 = true;
+                }
+                else
+                    flag3 = false;
+            }
+
+            Circle c3 = new Circle(x3, y3, r3);
+
+            printHead();
+            printMiddle();
+            Console.Write("║  1  ║");
+            printCircleParametrs(c1);
+            IsOneQuart(c1);
+
+            printMiddle();
+            Console.Write("║  2  ║");
+            printCircleParametrs(c2);
+            IsOneQuart(c2);
+
+            printMiddle();
+            Console.Write("║  3  ║");
+            printCircleParametrs(c3);
+            IsOneQuart(c3);
+            printEnd();
+
+            Console.WriteLine("Проверка на пересечение окружностей");
+            Console.WriteLine("1-ая и 2-ая: ");
+            IsIntersection(c1, c2);
+            Console.WriteLine("2-ая и 3-ая: ");
+            IsIntersection(c2, c3);
+            Console.WriteLine("1-ая и 3-ая: ");
+            IsIntersection(c1, c3);
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Вы можете изменить параметры 1-ой окружности");
+            Console.WriteLine("Что вы хотите изменить\n 1 - координаты центра окружности\n 2 - радиус окружности");
+            Console.WriteLine("Любой другой выбор оставит все без изменения!");
+            Console.Write("Ваш выбор: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Введите координаты центра 1-ой окружности");
+                    Console.Write("x1: ");
+                    int x1 = int.Parse(Console.ReadLine());
+                    Console.Write("y1: ");
+                    int y1 = int.Parse(Console.ReadLine());
+                    c1.ChangeCoords(x1, y1);
+                    break;
+                case 2:
+                    Console.WriteLine("Введите радиус 1-ой окружности");
+                    Console.Write("r1: ");
+                    int r1 = int.Parse(Console.ReadLine());
+                    c1.ChangeRadius(r1);
+                    break;
+                default:
+                    Console.WriteLine("Вы оставили параметры 1-ой окружности без изменения");
+                    break;
+            }
+
+            Console.WriteLine("Координаты 1-ой окружности!");
+            printHead();
+            printMiddle();
+            Console.Write("║  1  ║");
+            printCircleParametrs(c1);
+            IsOneQuart(c1);
+            printEnd();
+        }
+
+        static void printHead() // шапка
+        {
+            Console.WriteLine("╔═════╦═════════════╦════════╦═════════╦═════════════════════════╗");
+            Console.WriteLine("║№п/п ║    Центр    ║ Радиус ║  Длина  ║     Лежит ли в 1-ой     ║");
+            Console.WriteLine("║     ║             ║        ║         ║  координатной плоскости ║");
+        }
+
+        static void printMiddle() // середина
+        {
+            Console.WriteLine("╠═════╬═════════════╬════════╬═════════╬═════════════════════════╣");
+        }
+
+        static void printEnd() // конец
+        {
+            Console.WriteLine("╚═════╩═════════════╩════════╩═════════╩═════════════════════════╝");
+        }
+
+        static void printCircleParametrs(Circle c) // координаты Центра окружности
+        {
+            Console.Write($"  ({c.GetCenterX(),2};{c.GetCenterY(),2})    ║    {c.GetRadius(),1}   ║  {c.GetCircumference(),2:f2}   ║");
+        }
+
+        static void IsOneQuart(Circle c)
+        {
+            if (c.OneQuarterCoords())
+                Console.WriteLine("           ДА!           ║");
+            else
+                Console.WriteLine("           НЕТ!          ║");
+        }
+
+        static void IsIntersection(Circle c1, Circle c2)
+        {
+            if (!Circle.CircleIntersection(c1, c2))
+                Console.WriteLine("Пересекаются");
+            else
+                Console.WriteLine("Непересекаются");
+        }
+    }
+}
